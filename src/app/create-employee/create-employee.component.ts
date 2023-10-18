95175b70dc7e701d6e574b0ff516382a12250bb2
Denisa Andra Ionitoiu <denisa.andra.ionitoiu@ibm.com>
2023-10-18 12:53:00 +0300

    Connected Angular app with Add Employee REST API on the server part (stored employee form data into mysql database) and redirect to the employees page.

--- src/app/create-employee/create-employee.component.ts
+++ src/app/create-employee/create-employee.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Employee } from '../employee';
+import { EmployeeService } from '../employee.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-employee',
@@ -10,12 +12,28 @@ export class CreateEmployeeComponent {
 
   employee: Employee = new Employee();
 
-  constructor() {}
+  constructor(private employeeService: EmployeeService, private router: Router) {
+
+  }
 
   ngOnInit(): void {}
 
+  saveEmployee() {
+    this.employeeService.createEmployee(this.employee).subscribe( data => {
+      console.log(data);
+      this.goToEmployeeList();
+    },
+    error => console.log(error))
+  }
+
+  goToEmployeeList() {
+    this.router.navigate(['/employees'])
+  }
+
   onSubmit() {
+    // Will make REST api call and send the form data to the server's REST api and internally REST api will process this request and save the form data into mysql database.
     console.log(this.employee);
+    this.saveEmployee();
   }
 
 }
